2a03c63e6ad86d0752136353849e172a135c4076
Mohamed Shadhan <shadhanmoahmed13@gmail.com>
2025-04-10 22:24:53 +0300

    basic garden implemented

--- mindgarden/src/pages/Garden.jsx
+++ mindgarden/src/pages/Garden.jsx
@@ -1,5 +1,34 @@
+import { useState } from 'react'
+
 function Garden() {
-    return <div>🌸 Your garden will grow here.</div>
+
+  const [thoughts, setThoughts] = useState(() => {
+    // This runs only once on component mount
+    const stored = localStorage.getItem('thoughts')
+    return stored ? JSON.parse(stored) : []
+  })
+const plants = ["🪻", "🌹", "🌼", "🌷", "🌺", "🌻", "🌸", "🏵️", "🪷"]
+  return (
+    <div className="max-w-md mx-auto bg-white rounded-xl shadow-md p-4 space-y-4">
+      <h1 className="text-2xl font-bold">My Garden</h1>
+      <p className="text-gray-600">Welcome to your garden of thoughts!</p>
+      <div className="space-y-4">
+    
+  
+      </div>
+      {thoughts.length === 0 ? (
+        <p>{thoughts.length} plants in your garden</p>
+      ) : (
+        thoughts.map((t) => (
+          <div key={t.id}> 
+            <p>{t.text} this {plants[1]}</p>
+          </div>
+          ))
+      
+      ) }
+    </div>
+  )
+  
   }
   
   export default Garden
